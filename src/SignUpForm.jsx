a5b1fe7ff3e770aeebbc6c63e7e419327c57f9cf
Tim <tsclay9@gmail.com>
2020-09-08 19:26:26 -0400

    🥳 email inputs are debounced and verified async if unique

--- src/SignUpForm.jsx
+++ src/SignUpForm.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { TextField, Box, Button } from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
+import { TextField, Box, Button, FormHelperText } from '@material-ui/core'
 
 const styles = {
   wrapper: {
-    width: '70%',
+    width: '500px',
     margin: '0 auto'
   },
   form: {
@@ -14,12 +14,67 @@ const styles = {
   }
 }
 
+let timer
+
 function SignUpForm() {
+  const [payload, setPayload] = useState({
+    campaignUuid: '46aa3270-d2ee-11ea-a9f0-e9a68ccff42a'
+  })
+
+  const handleInputs = (e) => {
+    payload[e.target.name] = e.target.value
+    setPayload({ ...payload })
+  }
+
+  const verifyUniqueEmail = async () => {
+    const { email } = payload
+    if (!email) return
+    const response = await fetch('https://api.raisely.com/v3/check-user', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        campaignUuid: '46aa3270-d2ee-11ea-a9f0-e9a68ccff42a',
+        data: { email }
+      })
+    })
+    const json = await response.json()
+    console.log(json.data.status)
+    return json
+  }
+
+  useEffect(() => {
+    timer = setTimeout(() => {
+      verifyUniqueEmail()
+    }, 1500)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [payload.email])
+
+  const sendUserData = async (e) => {
+    e.preventDefault()
+    const response = await fetch('https://api.raisely.com/v3/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+    const json = response.json()
+    console.log('here is the response as json: ', json)
+  }
+
   return (
     <div id="signup-form" style={styles.wrapper}>
-      <form style={styles.form}>
+      <form onSubmit={sendUserData} style={styles.form}>
         <Box display="flex" flexDirection="column">
           <TextField
+            required
+            onChange={handleInputs}
             style={styles.inputs}
             type="text"
             name="firstName"
@@ -28,6 +83,8 @@ function SignUpForm() {
             variant="outlined"
           />
           <TextField
+            required
+            onChange={handleInputs}
             style={styles.inputs}
             type="text"
             name="lastName"
@@ -36,6 +93,8 @@ function SignUpForm() {
             variant="outlined"
           />
           <TextField
+            required
+            onChange={handleInputs}
             style={styles.inputs}
             autoComplete="off"
             type="email"
@@ -45,6 +104,8 @@ function SignUpForm() {
             variant="outlined"
           />
           <TextField
+            required
+            onChange={handleInputs}
             style={styles.inputs}
             type="password"
             name="password"
@@ -52,9 +113,13 @@ function SignUpForm() {
             label="Password"
             variant="outlined"
           />
-          <Button variant="contained" color="primary">
+          <Button disabled type="submit" variant="contained" color="primary">
             Signup
           </Button>
+          <FormHelperText>
+            Button will become clickable once all fields are filled with valid
+            credentials.
+          </FormHelperText>
         </Box>
       </form>
     </div>
